ca479c45132295d38db93209cf35f86fdde3da36
Matan <bennehem@g.jct.ac.il>
2025-12-29 13:57:50 +0200

    Refactor main.c with output
    Added hexCharToInt function and modified ProcessRxData to handle buffer count correctly. Enhanced main loop to transmit output to bus after processing.

--- main.c
+++ main.c
@@ -13,32 +13,58 @@ send to x from s (multi) functiom stored in extended memory waiting to send to b
 volatile uint8_t Snew = 0;
 volatile uint8_t buffer_count = 0;
 volatile bit buffer_flag = 0;
+int i;
 
-// Must be xdata to fit in memory , volatile make the compiler cheack if there changes
-volatile uint8_t xdata S_stream_expanded[MAX_PACKETS * HAMMING_R]; 
+volatile uint8_t xdata S_stream_expanded[Max_in_for_outpins * HAMMING_R]; // Must be xdata to fit in memory , volatile make the compiler cheack if there changes
+                                                 
+uint8_t xdata X_stream_output[Max_in_for_outpins * HAMMING_N]; // Output Buffer xdata - 15 * buffer size byte
 
-// Output Buffer xdata - 15 * buffer size byte
-uint8_t xdata X_stream_output[MAX_PACKETS * HAMMING_N];
+uint8_t R_config_list[Max_in_for_outpins];
 
-uint8_t R_config_list[MAX_PACKETS];
-
+hexCharToInt(uint8_t tempsnew) 
+			{
+                                           
+         if (tempsnew >= '0' && tempsnew <= '9') return (tempsnew - '0');             // Check if the character is a digit 0-9
+                                          
+         else if (tempsnew >= 'A' && tempsnew <= 'F') return (tempsnew - 'A' + 10);    // Check if the character is an uppercase letter A-F
+				
+         return -1;
+  	  } 
+bit ProcessRxData(uint8_t Snew)
+			{
+			 
+			 for (i = 0; i < HAMMING_R; i++)
+                {
+                    S_stream_expanded[(buffer_count * HAMMING_R) + i] = (Snew >> (HAMMING_R - 1 - i)) & 0x01;       // split the number in binary to 4 Bytes in buffer
+                }
+                
+                buffer_count++;
+                
+                if (buffer_count == 1) // only one char make interrupt
+                {
+                   return buffer_flag = 1;
+                }
+								return 0;
+  			}
 void main(void)
 {
-    uint8_t i;
-    
     GlobalINT();
     Timer3_Init();
     UART_Init();
-    
+	  Port_Init();
+	  
     while(1)
     {
         if (buffer_flag)
         {
+					
             ES = 0; 
             buffer_flag = 0; 
-            
-            get_multi_X_from_S((uint8_t*)S_stream_expanded, R_config_list, buffer_count, X_stream_output);         
-            buffer_count = 0;// Reset buffer
+					
+            get_multi_X_from_S((uint8_t*)S_stream_expanded, R_config_list, buffer_count, X_stream_output);   
+					
+					  transmit_X_to_bus(X_stream_output); // Send output to bus
+				  	buffer_count = 0;// Reset buffer
             ES = 1; // Re-enable Interrupts
         }
     }
